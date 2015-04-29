b7b3ed21a743532ee50d219cf15b1d0b4174db7d
tatiana frank <tatianafrank@gmail.com>
2015-04-28 20:11:00 -0400

    about to break something

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'faker'
 gem 'chronic'
 gem 'devise'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150428220422) do
+ActiveRecord::Schema.define(version: 20150428230311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "plans", force: :cascade do |t|
+    t.string   "start_time"
+    t.string   "end_time"
+    t.string   "venue"
+    t.string   "location"
+    t.string   "title"
+    t.string   "description"
+    t.string   "image"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "sites", force: :cascade do |t|
     t.datetime "created_at", null: false
