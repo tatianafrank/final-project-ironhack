143d19cbbb9b01ce9cb87c9840e7ae533c67d380
tatiana frank <tatianafrank@gmail.com>
2015-05-07 11:38:22 -0400

    fixed add event to calendar bug by adding calendar refresh function to fetch-schedule.js

--- app/assets/javascripts/fetch-schedule.js
+++ app/assets/javascripts/fetch-schedule.js
@@ -1,17 +1,19 @@
 function fetchSchedule (day) { 
+  //gets the api route which calls the myschedule method from api schedule controller
+  //this sets the daydate and passes it to schedule_for method in Event model 
   var request = $.get('/api/myschedule/' + day)
-
+  //when results from db call are returned schedule is passed.. 
+  //from the event model to the function
   request.done(function (schedule) {
-          console.log("Schedule: ", schedule)
-
 	//empty contents before appending 
-
 	$('.js-' + day + '-carousel').empty();
+  //iterates through the schedule array where events have been pushed from event model/schedule_for query
     schedule.forEach(function (event) {
+      //appends html and event detail interpolation to each day's carousel
       $('.js-' + day + '-carousel').append(eventHtmlBuilder(event))
 
     });
-
-    console.log('.js-' + day + '-carousel')
+    //used for addthisevent api to refresh the code on AJAX calls
+    addthisevent.refresh();
   });
 }
