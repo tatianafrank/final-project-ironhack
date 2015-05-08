ed53e0158286cc2c27764fc0ab479ac2aa14561f
tatiana frank <tatianafrank@gmail.com>
2015-05-08 14:04:21 -0400

    added ruby to message response

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -18,8 +18,8 @@ class MessagesController < ApplicationController
 						schedule.push(event)
 						next_available = event.end_time
 					r.Message 'Here are some events for this Friday. For Saturday or Sunday events, text back "saturday" or "sunday"
-						<%schedule.each do |event|%><%=event.start_time.in_time_zone("Eastern Time (US & Canada)").strftime('%l:%M%p')%>
-	                   - <%=event.end_time.in_time_zone("Eastern Time (US & Canada)").strftime('%l:%M%p')%> - <%=event.title%> - <%=event.location%>
+						<%schedule.each do |event|%><%=event.start_time.in_time_zone("Eastern Time (US & Canada)").strftime("%l:%M%p")%>
+	                   - <%=event.end_time.in_time_zone("Eastern Time (US & Canada)").strftime("%l:%M%p")%> - <%=event.title%> - <%=event.location%>
 	                   <%end%>'
 				elsif params[:Body]=='saturday'
 					r.Message 'Here are some events for this Saturday. For Friday or Sunday events, text back "friday" or "sunday"
