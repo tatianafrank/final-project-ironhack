a9d097c0e4f07b3d271043591aeb18dec0cccbd9
tatiana frank <tatianafrank@gmail.com>
2015-05-08 12:42:10 -0400

    added if else conditions for responding to texts through twilio

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,24 @@
 class MessagesController < ApplicationController
 	def reply
-		# account_sid = 'AC443bd92f18bbc08edfe72ae2a96a582f'
-		# auth_token = '47b1cd49700757d8292b1d0854d680fc'
-
-		# # set up a client to talk to the Twilio REST API
-		# @client = Twilio::REST::Client.new account_sid, auth_token
-		# @message=@client.messages.create
-		# alternatively, you can preconfigure the client like so
-
 	  	twiml = Twilio::TwiML::Response.new do |r|
-	    r.Message "Hey Monkey. Thanks for the message!"
+			if session[:have_messaged].nil?
+				session[:have_messaged] = true
+				r.Message 'Hello. Would you like to see plans for this weekend?'
+			else
+				if params[:body]=='yes' || params[:body]==='friday'
+					r.Message 'Here are some events for this Friday. For Saturday or Sunday events, text back "saturday" or "sunday"
+					7:00- Gallery Opening- 200 Broadway Ny, NY
+					9:30- Block Party- 58 Mercer St Ny, Ny'
+				elsif params[:body]==='saturday'
+					r.Message 'Here are some events for this Saturday. For Friday or Sunday events, text back "friday" or "sunday"
+					5:00- Golf Tournament- 700 Broadway Ny, NY
+					7:30- Wine Tasting- 28 Mercer St Ny, Ny'
+				elsif params[:body] === 'sunday'
+					r.Message 'Here are some events for this Sunday. For Friday or Saturday events, text back "friday" or "saturday"
+					3:00- Tennis Tournament- 400 Broadway Ny, NY
+					6:30- Movie Premiere- 88 Mercer St Ny, Ny'
+				end
+			end
 	  	end
 	  render xml: twiml.text
 	end
