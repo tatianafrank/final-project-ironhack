bde392ddb5cfff0d90db6691575de88ecde02544
tatiana frank <tatianafrank@gmail.com>
2015-05-08 13:08:44 -0400

    changed body to Body

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,15 +5,15 @@ class MessagesController < ApplicationController
 				session[:have_messaged] = true
 				r.Message 'Hello. Would you like to see plans for this weekend?'
 			else
-				if params[:body]=='yes' || params[:body]==='friday'
+				if params[:Body]=='yes' || params[:Body]=='friday'
 					r.Message 'Here are some events for this Friday. For Saturday or Sunday events, text back "saturday" or "sunday"
 					7:00- Gallery Opening- 200 Broadway Ny, NY
 					9:30- Block Party- 58 Mercer St Ny, Ny'
-				elsif params[:body]==='saturday'
+				elsif params[:Body]=='saturday'
 					r.Message 'Here are some events for this Saturday. For Friday or Sunday events, text back "friday" or "sunday"
 					5:00- Golf Tournament- 700 Broadway Ny, NY
 					7:30- Wine Tasting- 28 Mercer St Ny, Ny'
-				elsif params[:body] === 'sunday'
+				elsif params[:Body] == 'sunday'
 					r.Message 'Here are some events for this Sunday. For Friday or Saturday events, text back "friday" or "saturday"
 					3:00- Tennis Tournament- 400 Broadway Ny, NY
 					6:30- Movie Premiere- 88 Mercer St Ny, Ny'
