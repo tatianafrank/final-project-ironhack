8e176bf9df70d1d9174d326c0acd2c774f549e00
tatiana frank <tatianafrank@gmail.com>
2015-05-10 16:28:06 -0400

    twilio conditions in messages controller working in the browser

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,21 +6,33 @@ class MessagesController < ApplicationController
 				r.Message 'Hello. Would you like to see plans for this weekend?'
 			else
 				if params[:Body].downcase =='yes' || params[:Body]=='friday'
-					schedule=Event.schedule_for(daydate)
+					schedule=Event.schedule_for(Chronic.parse('friday'))
 					message=" "
 					   schedule.each do |event|
-					    message += "#{event.start_time.in_time_zone("Eastern Time (US & Canada)").strftime("%l:%M%p")}
+					    message += "
+					    #{event.start_time.in_time_zone("Eastern Time (US & Canada)").strftime("%l:%M%p")}
 	                   - #{event.end_time.in_time_zone("Eastern Time (US & Canada)").strftime("%l:%M%p")} - #{event.title} - #{event.location}"
 	                	end
 						r.Message 'Here are some events for this Friday. For Saturday or Sunday events, text back "saturday" or "sunday"' + message
 				elsif params[:Body]=='saturday'
-					r.Message 'Here are some events for this Saturday. For Friday or Sunday events, text back "friday" or "sunday"
-					5:00- Golf Tournament- 700 Broadway Ny, NY
-					7:30- Wine Tasting- 28 Mercer St Ny, Ny'
+					schedule=Event.schedule_for(Chronic.parse('saturday'))
+					message=" "
+					   schedule.each do |event|
+					    message += "
+					    #{event.start_time.in_time_zone("Eastern Time (US & Canada)").strftime("%l:%M%p")}
+	                   - #{event.end_time.in_time_zone("Eastern Time (US & Canada)").strftime("%l:%M%p")} - #{event.title} - #{event.location}"
+					   end
+					r.Message 'Here are some events for this Saturday. For Friday or Sunday events, text back "friday" or "sunday"'
+
 				elsif params[:Body] == 'sunday'
-					r.Message 'Here are some events for this Sunday. For Friday or Saturday events, text back "friday" or "saturday"
-					3:00- Tennis Tournament- 400 Broadway Ny, NY
-					6:30- Movie Premiere- 88 Mercer St Ny, Ny'
+					schedule=Event.schedule_for(Chronic.parse('sunday'))
+					message=" "
+					   schedule.each do |event|
+					    message += "
+					    #{event.start_time.in_time_zone("Eastern Time (US & Canada)").strftime("%l:%M%p")}
+	                   - #{event.end_time.in_time_zone("Eastern Time (US & Canada)").strftime("%l:%M%p")} - #{event.title} - #{event.location}"
+						end
+					r.Message 'Here are some events for this Sunday. For Friday or Saturday events, text back "friday" or "saturday"'
 				end
 			end
 	  	end
